5b263ff2b9c616fdf5ad2728c21c92287fca1160
Cemenayve <gabloooo@gmail.com>
2022-11-21 00:18:48 +0300

    Add OpenGL and GLFW libraries

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,11 @@
 #include "iostream"
+#include "GLFW/glfw3.h"
 
 int main(int argc, const char** argv)
 {
   std::cout << "Hello Fabric Simulator" << std::endl;
+
+  glfwInit();
+  glfwTerminate();
   return 0;
 }
